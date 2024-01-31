ffca0ffa41d0701054a9e6b73a05d2d0261c20a0
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-01-31 21:27:56 +0530

    Update and rename Reverse String to Reverse_String.cpp

--- Reverse_String.cpp
+++ Reverse_String.cpp
@@ -1,21 +1,21 @@
-#Reverse String (https://leetcode.com/problems/reverse-string)
+// #Reverse String (https://leetcode.com/problems/reverse-string)
 
-Write a function that reverses a string. The input string is given as an array of characters s.
-You must do this by modifying the input array in-place with O(1) extra memory.
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
 
-Example 1:
+// Example 1:
 
-Input: s = ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-Example 2:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
 
-Input: s = ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
  
-Constraints:
+// Constraints:
 
-1 <= s.length <= 105
-s[i] is a printable ascii character.
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
 
 Solution:
 
